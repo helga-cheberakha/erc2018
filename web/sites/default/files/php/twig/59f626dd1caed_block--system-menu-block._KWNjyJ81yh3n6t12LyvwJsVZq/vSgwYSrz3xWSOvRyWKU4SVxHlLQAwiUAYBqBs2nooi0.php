f1a0b38fc2f3aa0e9421bc5262e04b662287b996
Helga Cheberakha <helga.cheberakha@gmail.com>
2017-10-29 22:31:32 +0200

    Implements theme

---
+++ web/sites/default/files/php/twig/59f626dd1caed_block--system-menu-block._KWNjyJ81yh3n6t12LyvwJsVZq/vSgwYSrz3xWSOvRyWKU4SVxHlLQAwiUAYBqBs2nooi0.php
@@ -0,0 +1,124 @@
+<?php
+
+/* themes/custom/erc2018/templates/block/block--system-menu-block.html.twig */
+class __TwigTemplate_108b55d918e6b38ac861eebeb0d648893ac7d1398d5817a259b1db7592d0b470 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("@classy/block/block--system-menu-block.html.twig", "themes/custom/erc2018/templates/block/block--system-menu-block.html.twig", 1);
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "@classy/block/block--system-menu-block.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 10, "trans" => 17);
+        $filters = array("clean_id" => 10);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set', 'trans'),
+                array('clean_id'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 10
+        $context["show_anchor"] = ("show-" . \Drupal\Component\Utility\Html::getId($this->getAttribute(($context["attributes"] ?? null), "id", array())));
+        // line 11
+        $context["hide_anchor"] = ("hide-" . \Drupal\Component\Utility\Html::getId($this->getAttribute(($context["attributes"] ?? null), "id", array())));
+        // line 1
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 12
+    public function block_content($context, array $blocks = array())
+    {
+        // line 13
+        echo "  <div";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content_attributes"] ?? null), "addClass", array(0 => "content"), "method"), "html", null, true));
+        echo ">
+    ";
+        // line 15
+        echo "    <div class=\"menu-toggle-target menu-toggle-target-show\" id=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["show_anchor"] ?? null), "html", null, true));
+        echo "\"></div>
+    <div class=\"menu-toggle-target\" id=\"";
+        // line 16
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["hide_anchor"] ?? null), "html", null, true));
+        echo "\"></div>
+    <a class=\"menu-toggle\" href=\"#";
+        // line 17
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["show_anchor"] ?? null), "html", null, true));
+        echo "\">";
+        echo t("Show &mdash; @configuration.label", array("@configuration.label" => $this->getAttribute(($context["configuration"] ?? null), "label", array()), ));
+        echo "</a>
+    <a class=\"menu-toggle menu-toggle--hide\" href=\"#";
+        // line 18
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["hide_anchor"] ?? null), "html", null, true));
+        echo "\">";
+        echo t("Hide &mdash; @configuration.label", array("@configuration.label" => $this->getAttribute(($context["configuration"] ?? null), "label", array()), ));
+        echo "</a>
+
+    <nav id=\"navbar\" class=\"collapse navbar-collapse\">
+      <div class=\"container-fluid\">
+        ";
+        // line 22
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true));
+        echo "
+      </div>
+    </nav>
+  </div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/erc2018/templates/block/block--system-menu-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  89 => 22,  80 => 18,  74 => 17,  70 => 16,  65 => 15,  60 => 13,  57 => 12,  53 => 1,  51 => 11,  49 => 10,  11 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/custom/erc2018/templates/block/block--system-menu-block.html.twig", "/home/olha/Projects/freelance/erc2018/erc2018-web/web/themes/custom/erc2018/templates/block/block--system-menu-block.html.twig");
+    }
+}
