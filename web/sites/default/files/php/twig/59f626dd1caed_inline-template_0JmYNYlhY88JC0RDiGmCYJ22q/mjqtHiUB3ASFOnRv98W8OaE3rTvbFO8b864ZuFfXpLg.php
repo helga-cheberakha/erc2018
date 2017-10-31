677c1061979d0d9feec44ae2f41323847964c2b1
Helga Cheberakha <helga.cheberakha@gmail.com>
2017-10-31 22:44:19 +0200

    Implementing

---
+++ web/sites/default/files/php/twig/59f626dd1caed_inline-template_0JmYNYlhY88JC0RDiGmCYJ22q/mjqtHiUB3ASFOnRv98W8OaE3rTvbFO8b864ZuFfXpLg.php
@@ -0,0 +1,82 @@
+<?php
+
+/* {# inline_template_start #}<h3>{{ title }}</h3>
+<div class="post-date">{{ created }}</div> */
+class __TwigTemplate_77c7073e9a513545742e2b7eb4c41a8cbab62b6a4d7c497b2a96fa3464dfc47d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<h3>";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title"] ?? null), "html", null, true));
+        echo "</h3>
+<div class=\"post-date\">";
+        // line 2
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["created"] ?? null), "html", null, true));
+        echo "</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<h3>{{ title }}</h3>
+<div class=\"post-date\">{{ created }}</div>";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  49 => 2,  44 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "{# inline_template_start #}<h3>{{ title }}</h3>
+<div class=\"post-date\">{{ created }}</div>", "");
+    }
+}
